84dd9341822baa3c3283152a43951747681755d8
Gabriel Wagner <wagner.542@gmail.com>
2025-09-23 20:44:08 -0400

    adding optional port parameter to GetConnectionString
    - this is for integration testing purposes
    - during integration test postgres db is created on a random port

--- HelpDeskLibrary/DatabaseConfig.cs
+++ HelpDeskLibrary/DatabaseConfig.cs
@@ -2,12 +2,16 @@
 
 public static class DatabaseConfig
 {
-	public static string GetConnectionString()
+	public static string GetConnectionString(int? port = null)
 	{
 		var password = Environment.GetEnvironmentVariable("HDADMINPASSWORD");
 
-		return password == null
-			? throw new InvalidOperationException("Failed to get HDADMINPASSWORD environment variable.")
-			: $"Host=localhost;Database=HelpDesk;Username=hdadmin;Password={password};Pooling=true;MinPoolSize=5;MaxPoolSize=100;";
+		if (password == null)
+			throw new InvalidOperationException("Failed to get HDADMINPASSWORD environment variable.");
+
+		if (port == null)
+			return $"Host=localhost;Database=HelpDesk;Username=hdadmin;Password={password};Pooling=true;MinPoolSize=5;MaxPoolSize=100;";
+
+		return $"Host=localhost;Port={port};Database=HelpDesk;Username=hdadmin;Password={password};Pooling=true;MinPoolSize=5;MaxPoolSize=100;";
 	}
 }
